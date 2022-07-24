f7cad65d2e26faf19930e612096c3edf7e75be57
Ming Li <Mingrkli@gmail.com>
2022-07-24 09:43:59 -0700

    Update
    Added a new page, added phone response, and finished the login / Signup popup to the best of my ability without back-end

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,6 +1,8 @@
 // Nav bar
 const navBtn = document.querySelectorAll('#nav-bar button')
 const pageSections = document.querySelectorAll('.page-sections')
+// Why Join Imgs
+const whyJoinImgs = document.querySelectorAll('#why-join-imgs div img')
 
 // Click btn in nav-bar to change to that class section
 for (let btn = 0; btn < navBtn.length; btn++) {
@@ -15,3 +17,15 @@ for (let btn = 0; btn < navBtn.length; btn++) {
 
     })
 }
+
+whyJoinImgs.forEach(e => {
+    e.addEventListener('mouseover', () => {
+        // Removes all enlarge class when hover to other img
+        whyJoinImgs.forEach( img => {
+            img.classList.remove('enlarge')
+        })
+
+        // Adds class enlarge
+        e.classList.add('enlarge')
+    })
+});
